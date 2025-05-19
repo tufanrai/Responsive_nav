4270151170c04858b28f0aa0ece768da3eec8f1c
TufanRai <thulungtufan16@gmail.com>
2025-05-19 10:31:23 +0545

    responsive navigation bar using html css and js

---
+++ script.js
@@ -0,0 +1,11 @@
+function sidebar_open() {
+        const side_nav = document.querySelector('.side-bar')
+        side_nav.style = 'display: flex;'
+}
+
+function sidebar_close() {
+    const side_nav = document.querySelector('.side-bar')
+        side_nav.style = 'display: none;'
+}
+
+
